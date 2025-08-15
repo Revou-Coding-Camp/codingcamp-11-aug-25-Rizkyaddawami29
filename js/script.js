33e8698cad0fc25e21c70caf1e84cf22d81915d2
Muhammad Rizky Addawami <rizkyaddawami029@gmail.com>
2025-08-15 14:24:22 +0700

    Perubahan ketiga

--- js/script.js
+++ js/script.js
@@ -10,8 +10,14 @@ function welcomespeech(){
 
 function sendmessage(){
     let message = document.getElementById('user-message').value;
-    if(message != '' ){
-        alert("message sent: " + message);
+    let name = document.getElementById('user-name').value;
+    let gender = document.querySelector('input[name="gender"]:checked');
+    let genderValue = gender ? gender.value : "Belum dipilih"
+    let dob = document.getElementById("dob").value;
+    let now = new Date();
+    let waktuInput = now.toLocaleString();
+    if(message != '' || name != ''){
+        alert("message sent: " + message +"\n"+ "your name: " + name + "\n" + "gender: " + genderValue + "\n" + "Date of Birth: " + dob + "\n" + "Input time: " + waktuInput);
     } else{
         alert("please enter a message before sending");
     }
